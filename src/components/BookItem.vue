7bdceeda97d8a19ecadfb855bf2e29de3d67843a
Ivan Sinyakov <bglaze@mail.ru>
2023-05-26 00:47:33 +1000

    completed order book

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-const { quantity, price, type } = defineProps<{ quantity: number; price: number; type: string }>();
+const { quantity, price, type, avgAmount } = defineProps<{
+  quantity: number;
+  price: number;
+  type: string;
+  avgAmount: number;
+}>();
+
+const formattedPrice = (num: number) => {
+  const formatter = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" });
+  return formatter.format(num);
+};
 </script>
 
 <template>
   <div class="item">
+    <div
+      class="item__progress"
+      :style="{
+        transform: `translateX(-${Math.min((quantity / avgAmount) * 100, 100)}%)`,
+        backgroundColor: type === 'bid' ? '#3b1919' : '#1a2b28',
+      }"
+    ></div>
     <div class="item__wrapper">
       <div
         class="item__price"
         :class="[type === 'bid' ? 'red-color' : 'green-color']"
       >
-        {{ price }}
+        {{ price.toFixed(2) }}
       </div>
-      <div class="item__quantity">{{ quantity }}</div>
-      <div class="item__total">{{ price * quantity }}</div>
+      <div class="item__quantity">{{ quantity.toFixed(5) }}</div>
+      <div class="item__total">{{ formattedPrice(price * quantity) }}</div>
     </div>
   </div>
 </template>
@@ -20,18 +37,44 @@ const { quantity, price, type } = defineProps<{ quantity: number; price: number;
 <style scoped lang="scss">
 .item {
   width: 100%;
-  background-color: #1f252a;
+  background-color: #14181b;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  &__progress {
+    position: absolute;
+    top: 0;
+    left: 100%;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+  }
+
   &__wrapper {
+    position: relative;
+    z-index: 3;
+    padding: 5px 20px;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: white;
+    color: rgb(201, 210, 219);
+  }
+  &__price {
+    flex: 1 1 0%;
+    text-align: left;
+  }
+  &__quantity {
+    flex: 1 1 0%;
+    text-align: right;
+  }
+  &__total {
+    flex: 1 1 0%;
+    text-align: right;
   }
 }
 
 .red-color {
   color: #f0455b;
 }
+
 .green-color {
   color: #0fc47d;
 }
--- src/components/OrderBook.vue
+++ src/components/OrderBook.vue
@@ -1,46 +1,87 @@
 <script setup lang="ts">
-import { ref, onMounted, computed, onBeforeUnmount, onBeforeUpdate } from "vue";
-import { useStore } from "@/stores/mainStore";
-import BookItem from "./BookItem.vue";
+import { ref, onMounted, computed, onBeforeUnmount, onBeforeUpdate, watch } from "vue";
+import { useStore, type Quote } from "@/stores/mainStore";
+import Loading from "@/components/Loading.vue";
+import BookItem from "@/components/BookItem.vue";
 
 const store = useStore();
+
+interface WSData {
+  e: string;
+  E: string;
+  s: string;
+  U: number;
+  u: number;
+  b: string[][];
+  a: string[][];
+  c: string;
+}
 
 const bids = computed(() => store.state.bids);
 const asks = computed(() => store.state.asks);
 const orders = computed(() => store.state.orders);
 const chosenSymbol = computed(() => store.state.chosenSymbol);
+const avgBidsAmmount = ref(0);
+const avgAsksAmmount = ref(0);
+const currentPrice = ref(0);
+
 let websocket: WebSocket | null = null;
+let wsPrice: WebSocket;
 
 const connectToWebSocket = () => {
   websocket = new WebSocket(
     `wss://stream.binance.com:9443/ws/${chosenSymbol.value.toLocaleLowerCase()}@depth`
   );
+  wsPrice = new WebSocket(
+    `wss://stream.binance.com:9443/ws/${chosenSymbol.value.toLocaleLowerCase()}@ticker`
+  );
+  wsPrice.onmessage = handleWebSocketMessage;
   websocket.onmessage = handleWebSocketMessage;
 };
 
 const handleWebSocketMessage = (event: MessageEvent) => {
-  const data = JSON.parse(event.data);
+  const data: WSData = JSON.parse(event.data);
   if (data.e === "depthUpdate") {
     updateOrderBook(data);
   }
+  if (data.e === "24hrTicker") {
+    currentPrice.value = +data.c;
+  }
 };
 
-const updateOrderBook = (data: any) => {
-  const bidsData = data.b.filter((order: any) => parseFloat(order[1]) > 0);
-  const asksData = data.a.filter((order: any) => parseFloat(order[1]) > 0);
+const updateOrderBook = (data: WSData) => {
+  const bidsData = data.b.filter((order: string[]) => parseFloat(order[1]) > 0);
+  const asksData = data.a.filter((order: string[]) => parseFloat(order[1]) > 0);
+
+  const bids = bidsData.map(
+    (order: string[]): Quote => ({ price: +order[0], quantity: +order[1] })
+  );
+  const asks = asksData.map(
+    (order: string[]): Quote => ({ price: +order[0], quantity: +order[1] })
+  );
+
+  avgAsksAmmount.value =
+    asks.reduce((acc: number, item: Quote) => (acc += item.quantity), 0) / asks.length;
+  avgBidsAmmount.value =
+    bids.reduce((acc: number, item: Quote) => (acc += item.quantity), 0) / bids.length;
 
   store.dispatch(
     "setBids",
-    bidsData.map((order: any) => ({ price: order[0], quantity: order[1] }))
+    bids.slice(0, 15).sort((a: Quote, b: Quote) => b.price - a.price)
   );
   store.dispatch(
     "setAsks",
-    asksData.map((order: any) => ({ price: order[0], quantity: order[1] }))
+    asks.slice(0, 15).sort((a: Quote, b: Quote) => b.price - a.price)
   );
 };
 
+watch(
+  () => currentPrice.value,
+  (newPrice: number, oldPrice: number) => {}
+);
+
 onMounted(() => {
-  console.log(1);
+  console.log("mounted");
   connectToWebSocket();
 });
 
@@ -50,86 +91,74 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <!-- <div>
-    <h1>Order Book</h1>
-    <div class="order-book">
-      <div class="bids">
-        <h2>Bids</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Price</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="bid in bids"
-              :key="bid.price"
-            >
-              <td>{{ bid.price }}</td>
-              <td>{{ bid.quantity }}</td>
-            </tr>
-          </tbody>
-        </table>
+  <div class="orders">
+    <Loading v-if="!(bids.length > 0)" />
+    <div v-else>
+      <h1 class="orders__title">Orders book</h1>
+      <div class="orders__book">
+        <div class="orders__header">
+          <div class="orders__price">Price</div>
+          <div class="orders__quantity">Quantity</div>
+          <div class="orders__total">Total($USD)</div>
+        </div>
       </div>
-      <div class="asks">
-        <h2>Asks</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Price</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="ask in asks"
-              :key="ask.price"
-            >
-              <td>{{ ask.price }}</td>
-              <td>{{ ask.quantity }}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="orders__bids">
+        <BookItem
+          v-for="bid in bids"
+          :avg-amount="avgBidsAmmount"
+          type="bid"
+          :quantity="bid.quantity"
+          :price="bid.price"
+        />
+      </div>
+      <div class="orders__info">
+        <div class="orders__price">{{ currentPrice }}</div>
+        <div class="orders__spread">spread: 0.4%</div>
+      </div>
+      <div class="orders__asks">
+        <BookItem
+          v-for="ask in asks"
+          :avg-amount="avgAsksAmmount"
+          type="ask"
+          :quantity="ask.quantity"
+          :price="ask.price"
+        />
       </div>
     </div>
-  </div> -->
-  <div class="orders">
-    <BookItem
-      type="bid"
-      :quantity="100"
-      :price="25673"
-    />
   </div>
 </template>
 
-<style scoped>
-.order-book {
-  display: flex;
-}
-
-.bids,
-.asks {
-  flex: 1;
-}
-
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-th,
-td {
-  padding: 8px;
-  text-align: left;
-}
-
-th {
-  background-color: #eee;
-}
-
-tr:nth-child(even) {
-  background-color: #f2f2f2;
+<style scoped lang="scss">
+.orders {
+  margin-top: 40px;
+  &__header {
+    background-color: #1f252a;
+    padding: 5px 20px 5px 20px;
+    display: flex;
+    color: rgb(201, 210, 219);
+    border-bottom: 1px solid #939393;
+  }
+  &__price {
+    flex: 1 1 0%;
+    text-align: left;
+  }
+  &__quantity {
+    flex: 1 1 0%;
+    text-align: right;
+  }
+  &__total {
+    flex: 1 1 0%;
+    text-align: right;
+  }
+  &__info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #1f252a;
+    border: 1px 0 solid #939393;
+    color: rgb(201, 210, 219);
+    font-size: 22px;
+    padding: 10px 20px;
+  }
 }
 </style>
